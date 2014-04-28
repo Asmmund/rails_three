1af83675e81f9d161e28a9c52f56a1693055d5a9
Asmmund <a.yermolenko@svitla.com>
2014-04-28 21:33:38 +0300

    creation

---
+++ app/controllers/seconds_controller.rb
@@ -0,0 +1,19 @@
+class SecondsController < ApplicationController
+  def index
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsThree::Application.config.secret_token = 'f3cd4888fe4cbca6099149feabc72ca3947f095bd1f681288981c43bb908863b7c10eacc1985c44ad0d8ecb0d4157152dba035ab7000dba34c741f069b778b1b'
